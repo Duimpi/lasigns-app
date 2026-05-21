6366f369873e48e0a2490a50f155442e718facaa
Duimpi <damionhoniball@yahoo.com>
2026-05-21 13:24:14 +0200

    hardcode

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: 'https://wbkecmtcygiwiebrculy.supabase.co',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6India2VjbXRjeWdpd2llYnJjdWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMTQ2MjksImV4cCI6MjA5NDg5MDYyOX0.6fDe4128i9OYmAwlLAclZqaBx1umk4s49Lp4DKiVXK0',
+    SUPABASE_URL: 'https://wbkecmtcygiwiebrculy.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6India2VjbXRjeWdpd2llYnJjdWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMTQ2MjksImV4cCI6MjA5NDg5MDYyOX0.6fDe4128i9OYmAwlLAclZqaBx1umk4s49Lp4DKiVXK0',
   },
 }
 
