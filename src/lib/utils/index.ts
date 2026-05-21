ca1eb115ee5903dae237c242eaabc1ed6d6a35ca
Duimpi <damionhoniball@yahoo.com>
2026-05-21 17:59:44 +0200

    Fix type errors

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -58,7 +58,8 @@ export function generateId(): string {
   return crypto.randomUUID()
 }
 
-export function debounce<T extends (...args: unknown[]) => unknown>(
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => any>(
   fn: T,
   delay: number
 ): (...args: Parameters<T>) => void {
